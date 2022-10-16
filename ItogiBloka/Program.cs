c36ac46d0ad2840427aba2db3db5a703b0706a36
Julia <yuliabir@mail.ru>
2022-10-17 00:25:34 +0400

    Программа

--- ItogiBloka/Program.cs
+++ ItogiBloka/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Promt(string message)
+{
+    System.Console.WriteLine(message);
+    string[] result = Console.ReadLine().Split(" ");
+    return result;
+}
+
+int n= 3;
+		
+		string[] GetSNewArray(string[] array)
+		{
+			int length = array.Length;			
+			string[] result = new string[length];
+			int count = 0;
+			
+			for (int i = 0; i < length; i++)
+			{
+				if(array[i].Length <= n)
+				{
+					result[count] = array[i];
+					count++;
+				}
+			}
+			Array.Resize(ref result, count);
+			
+			return result;
+		}
+		
+		void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+          System.Console.Write($"{array[i]}\t");
+        }
+        System.Console.WriteLine();
+}
+		string[] array = Promt("Введите значения через пробел  ");
+string[] newArray = GetSNewArray(array);
+        PrintArray(newArray);
