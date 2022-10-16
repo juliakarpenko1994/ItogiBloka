8ec6b7b3246c73285190a7ed4111b78b9328f4f9
Julia <yuliabir@mail.ru>
2022-10-17 00:27:24 +0400

    Итоговое задание

--- ItogiBloka/Program.cs
+++ ItogiBloka/Program.cs
@@ -5,35 +5,35 @@
     return result;
 }
 
-int n= 3;
-		
-		string[] GetSNewArray(string[] array)
-		{
-			int length = array.Length;			
-			string[] result = new string[length];
-			int count = 0;
-			
-			for (int i = 0; i < length; i++)
-			{
-				if(array[i].Length <= n)
-				{
-					result[count] = array[i];
-					count++;
-				}
-			}
-			Array.Resize(ref result, count);
-			
-			return result;
-		}
-		
-		void PrintArray(string[] array)
+int n = 3;
+
+string[] GetSNewArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
+
+void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-          System.Console.Write($"{array[i]}\t");
-        }
-        System.Console.WriteLine();
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
 }
-		string[] array = Promt("Введите значения через пробел  ");
+string[] array = Promt("Введите значения через пробел  ");
 string[] newArray = GetSNewArray(array);
-        PrintArray(newArray);
+PrintArray(newArray);
